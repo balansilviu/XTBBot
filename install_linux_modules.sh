339b0b79701367e2693060104586ed9ba56df048
Silviu Balan <balan.silviu.dan@gmail.com>
2025-01-22 16:11:20 +0000

    Linux setup script added

---
+++ install_linux_modules.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install python3-pip -y
+python3 -m pip install customtkinter
+sudo apt install python3-tk -y
+python3 -m pip install numpy
+python3 -m pip install websocket-client
+sudo apt install -y build-essential qt5-default qttools5-dev-tools
+python3 -m pip install --upgrade pip setuptools wheel
+python3 -m pip install testresources
+python3 -m pip install PyQt5
+
